80fb974683939266b89f4ef9f5d5ac2f79273976
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-06-22 00:52:48 +0200

    JS

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+// Find elements
+const screen = document.querySelector(".screen");
 
+const buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e);
+    switch (e.target.innerHTML) {
+      default:
+        screen.innerHTML += e.target.innerHTML;
+        break;
+      case "RESET":
+        screen.innerHTML = "";
+        break;
+      case "DEL":
+        screen.innerHTML = screen.innerHTML.slice(0, -1);
+        break;
+      case "=":
+        screen.innerHTML = eval(screen.innerHTML);
+        break;
+    }
+  });
+});
+
+
+function(){
+    
+    
+
+
+
+
+ console.log();
+}
